61b818f1f94aa3593631a9325c88d09654551a04
johnny <johnny1194@hotmail.com>
2021-09-08 11:23:53 -0400

    Adding relationships to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
   belongs_to :console
+  has_many :gameplays
+  has_many :players, through :gameplay
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+    has_many :gameplays
+    has_many :games, through :gameplay
 end

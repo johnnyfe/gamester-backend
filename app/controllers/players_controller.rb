6f8a1860e136b253b23ed91b979a98a91da35a57
johnny <johnny1194@hotmail.com>
2021-09-09 09:55:43 -0400

    player routing and cors confi

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,6 @@
 class PlayersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
     def index
         players = Player.all
@@ -6,10 +8,43 @@ class PlayersController < ApplicationController
     end
 
     def show
-        player = Player.find_by(id: params[:id])
+        player = find_player
         render json: player, except: [:updated_at, :created_at]
     end
 
-    
+    def create
+        player = Player.create!(player_params)
+        render json: player
+    end
 
+    def updated
+        player = find_player
+        player.update(player_params)
+        render json: player
+    end
+
+    def destroy
+        player = find_player
+        player.destroy
+        head :no_content
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :age, :years_of_experience, :country, :photo_url)
+    end
+
+    def find_player
+        Player.find(params[:id])
+    end
+
+    def render_not_found
+        render json: { error: "player not found"}, status: :render_not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :render_unprocessable_entity
+    end
+    
 end

072cc54ec68617ec871fac3daa3bddeb8e3d5280
johnny <johnny1194@hotmail.com>
2021-09-08 21:00:29 -0400

    adding index route

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,10 @@
 class PlayersController < ApplicationController
+
+    def index
+        players = Player.all
+        render json: players, except: [:updated_at, :created_at]
+    end
+
+    
+
 end

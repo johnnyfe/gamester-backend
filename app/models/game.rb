237e2a31fa2117cb0fe70a87e6edf7ff523b7988
johnny <johnny1194@hotmail.com>
2021-09-09 21:56:10 -0400

    adding game validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,12 @@ class Game < ApplicationRecord
   belongs_to :console
   has_many :gameplays, dependent: :destroy
   has_many :players, through: :gameplay
+
+  validates :name, presence: true, uniqueness: true
+  validates :category, presence: true
+  validates :cost, presence: true, numericality: { only_integer: true, greater_than: 9, less_than: 100 }
+  validates :rate, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 11 }
+  validates :average_length, presence: true, numericality: { only_float: true, greater_than: 10, less_than: 100 }
+  validates :img_url, presence: true, uniqueness: true
+  validates :storage_space, presence: true, numericality: { only_float: true, greater_than: 0, less_than: 250 }
 end

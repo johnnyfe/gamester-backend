7ee02def020aaf3d4c257c840faac1d49ea045a6
johnny <johnny1194@hotmail.com>
2021-09-09 22:14:50 -0400

    adding consoles validations

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,3 +1,8 @@
 class Console < ApplicationRecord
     has_many :games, dependent: :destroy
+
+    validates :name, presence: true, uniqueness: true
+    validates :cost, presence: true, numericality: { only_integer: true, greater_than: 150, less_than: 1000 }
+    validates :img_url, presence: true, uniqueness: true
+    validates :storage_capacity, presence: true, numericality: { only_integer: true , greater_than: 1, less_than: 5000}
 end

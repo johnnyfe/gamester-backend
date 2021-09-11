e7da61701cf9ea256e46ac07efcc42b5aef07fa3
johnny <johnny1194@hotmail.com>
2021-09-10 23:18:52 -0400

    updating gameplay serializer

--- app/serializers/gameplay_serializer.rb
+++ app/serializers/gameplay_serializer.rb
@@ -1,3 +1,3 @@
 class GameplaySerializer < ActiveModel::Serializer
-    attributes :id, :game_time, :likes, :video_url, :title
+    attributes :id, :game_time, :likes, :video_url, :title, :game, :player
 end
